1d98ba5acddc995ad4f904465876befdbf51122d
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-05-16 12:38:08 -0400

    The navbar for smartphones has been completed

--- frontend/src/components/navbar/mobile/index.tsx
+++ frontend/src/components/navbar/mobile/index.tsx
@@ -1,11 +1,41 @@
+'use client'
 
-export default function NavBarMovile (){
+import Link from "next/link";
+import { FiHome, FiSearch, FiPlus, FiHeart, FiUser } from "react-icons/fi";
+import { usePathname } from "next/navigation";
+
+export default function NavBarMovile(): JSX.Element {
+
+  const pathname = usePathname();
+
   return (
-    <nav className="flex flex-row justify-between items-center fixed bottom-0 w-full bg-color3 py-7 px-4 gap-4 text-white text-xl">
-      <p>home</p>
-      <p>search</p>
-      <p>new publish</p>
-      <p>profile</p>
+    <nav className="flex flex-row justify-center items-center fixed bottom-0 w-full bg-color3 py-7 px-4 gap-14 text-white text-3xl">
+
+      <Link href="/" className={`${pathname === '/' ? 'text-white' : 'text-slate-400'}`}>
+        <p>
+          <FiHome />
+        </p>
+      </Link>
+      <Link href="/search" className={`${pathname === '/search' ? 'text-white' : 'text-slate-400'}`}>
+        <p>
+          <FiSearch />
+        </p>
+      </Link>
+
+      <p>
+        <FiPlus />
+      </p>
+      
+      <p>
+        <FiHeart />
+      </p>
+
+      <Link href="/profile" className={`${pathname === '/profile' ? 'text-white' : 'text-slate-400'}`}>
+        <p>
+          <FiUser />
+        </p>
+      </Link>
+
     </nav>
   )
 };
