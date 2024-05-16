e67e000edd4e6554139979312bc9e36eef58eb0a
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-05-16 13:45:17 -0400

    modified routes of te navbar and modified icon

--- frontend/src/components/navbar/mobile/index.tsx
+++ frontend/src/components/navbar/mobile/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from "next/link";
-import { FiHome, FiSearch, FiPlus, FiHeart, FiUser } from "react-icons/fi";
+import { FiHome, FiSearch, FiPlusCircle, FiHeart, FiUser } from "react-icons/fi";
 import { usePathname } from "next/navigation";
 
 export default function NavBarMovile(): JSX.Element {
@@ -10,32 +10,34 @@ export default function NavBarMovile(): JSX.Element {
 
   return (
     <nav className="flex flex-row justify-center items-center fixed bottom-0 w-full bg-color3 py-7 px-4 gap-14 text-white text-3xl">
-
       <Link href="/" className={`${pathname === '/' ? 'text-white' : 'text-slate-400'}`}>
         <p>
           <FiHome />
         </p>
       </Link>
-      <Link href="/search" className={`${pathname === '/search' ? 'text-white' : 'text-slate-400'}`}>
+      <Link href="/explore" className={`${pathname === '/search' ? 'text-white' : 'text-slate-400'}`}>
         <p>
           <FiSearch />
         </p>
       </Link>
 
-      <p>
-        <FiPlus />
-      </p>
+      <Link href='/post'>
+        <p>
+          <FiPlusCircle />
+        </p>
+      </Link>
       
-      <p>
-        <FiHeart />
-      </p>
+      <Link href='/notifications'>
+        <p>
+          <FiHeart />
+        </p>
+      </Link>
 
       <Link href="/profile" className={`${pathname === '/profile' ? 'text-white' : 'text-slate-400'}`}>
         <p>
           <FiUser />
         </p>
       </Link>
-
     </nav>
   )
 };
