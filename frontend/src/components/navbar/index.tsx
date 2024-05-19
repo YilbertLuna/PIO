6a652ee66224bcdc12b7a79ebe19baafe3666e6e
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-05-18 21:49:21 -0400

    giving the navbar a background

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -4,7 +4,7 @@ import NavBarDesktop from "./desktop"
 export default function NavBar() {
     return(
         <div>
-            <div className="md:hidden">
+            <div className="md:hidden bg-color3">
                 <NavBarMovile />
             </div>
             <div className="hidden md:flex fixed top-0 h-full">
