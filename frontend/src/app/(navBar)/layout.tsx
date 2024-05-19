7c89330e016b466b1793ee3aa5419cd1afc01bc7
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-05-18 21:59:16 -0400

    the styles were removed

--- frontend/src/app/(navBar)/layout.tsx
+++ frontend/src/app/(navBar)/layout.tsx
@@ -4,9 +4,9 @@ export default function Layout({ children }: {
     children: React.ReactNode
   }) {
     return (
-      <section className="md:flex flex-row justify-between items-center w-full">
-        <NavBar />
+      <section>
         {children}
+        <NavBar />
       </section>
     )
 }
