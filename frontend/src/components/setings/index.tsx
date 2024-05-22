fc3d37386c2df7ad0e5ff84f1ddf16de2ac07f0c
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-05-22 16:54:39 -0400

    adding hidden

--- frontend/src/components/setings/index.tsx
+++ frontend/src/components/setings/index.tsx
@@ -3,7 +3,7 @@ import { FiAlignRight } from "react-icons/fi";
 
 export default function Setings() {
     return(
-        <div className="flex flex-row top-2 fixed w-[100%] justify-center items-center">
+        <div className="flex flex-row top-2 fixed w-[100%] justify-center items-center md:hidden">
             <div className="flex justify-between items-center w-[50%] bg-color3  rounded-3xl p-4 text-center">
                 <figure className="flex justify-center items-center">
                     <Image src="/PIO_Blanco.png" alt="PIO logo" width={50} height={50} />
