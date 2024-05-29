b627551bd4f07183f0af6c95d0b4cd23bf39de8d
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-05-29 13:32:29 -0400

    Implementation of the route for user registration and user login

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import userRouter from './router/user.routes.js';
+
 const app = express();
 
 app.use(morgan('dev'))
 app.use(express.json())
+
+app.use('/api', userRouter)
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
