3fe172136ea4bbf9d53c365f03d11ec8bbe753db
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-06-04 10:24:01 -0400

    modifed file

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,10 @@
 import 'dotenv/config'
 import app from './src/server.js'
 
-const port = process.env.PORT || 3030
-
-
 const serverRun = () => {
 
-    app.listen(port, () => {
-        console.log(`server is running successfully 🟢 on port ${port}`)
+    app.listen(process.env.PORT, () => {
+        console.log(`server is running successfully 🟢 on port ${process.env.PORT}`)
     })
 
 }
