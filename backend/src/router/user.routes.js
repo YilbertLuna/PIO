ee020f4571eaadb979e4d3ceb08e2b6b7df5bdcf
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-06-06 21:26:51 -0400

    Route to register the user and save it in the database

--- backend/src/router/user.routes.js
+++ backend/src/router/user.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { login, register } from "../controllers/user.controller.js";
+import { register } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.post('/register', register)
-userRouter.post('/login', login)
 
 export default userRouter
