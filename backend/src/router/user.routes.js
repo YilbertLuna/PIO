fbc9b210a1803fa26075c5c92395305538b014b1
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-06-10 13:29:33 -0400

    feat(register): adding login, getUsers and updateUser

--- backend/src/router/user.routes.js
+++ backend/src/router/user.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { register } from "../controllers/user.controller.js";
+import { register, getUsers, updateUser, login} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.post('/register', register)
+userRouter.post('/login', login)
+userRouter.get('/getUsers', getUsers)
+userRouter.put('/updateUser/:id', updateUser)
 
 export default userRouter
