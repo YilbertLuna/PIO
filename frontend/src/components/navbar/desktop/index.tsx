dea80c2117e180d3eb623a18a8cdc33b870f33f1
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-06-17 18:04:45 -0400

    style(front): change style navbar

--- frontend/src/components/navbar/desktop/index.tsx
+++ frontend/src/components/navbar/desktop/index.tsx
@@ -13,52 +13,44 @@ export default function NavBarDesktop(): JSX.Element {
 
     return (
         <Sidebar className="rounded-none border-r border-black bg-color3">
-            <Sidebar.Body className='flex flex-col justify-center items-start space-y-7'>
+            <Sidebar.Body className='flex flex-col justify-center items-center space-y-7'>
                 <Sidebar.Item>
                     <Link href="/" className={`${pathname === '/' ? 'text-white' : 'text-slate-400'} flex items-center gap-2 `}>
                         <FiHome className='text-3xl' />
-                        Home
                     </Link>
                 </Sidebar.Item>
                 
                 <Sidebar.Item>
                     <Link href="/explore" className={`${pathname === '/explore' ? 'text-white' : 'text-slate-400'} flex items-center gap-2 `}>
                        <FiSearch className='text-3xl' />
-                       Search
                     </Link>
                 </Sidebar.Item>
 
                 <Sidebar.Item>
-                    <span className='flex items-center gap-2 text-slate-400 '>
+                    <span className='flex items-center gap-2 text-slate-400 cursor-pointer'>
                         <span className='text-3xl'>
                             <ModalComponent />
                         </span>
-                        New post
                     </span>
                 </Sidebar.Item>
                 
                 <Sidebar.Item>
                     <Link href="/notifications" className={`${pathname === '/notifications' ? 'text-white' : 'text-slate-400'} flex items-center gap-2 `}>
                         <FiHeart className='text-3xl' />
-                        notifications
                     </Link>
                 </Sidebar.Item>
             </Sidebar.Body>
 
             <Divider className="my-4" />
             
-            <Sidebar.Footer className="flex flex-col justify-between items-start gap-2">
+            <Sidebar.Footer className="flex flex-col justify-between items-center gap-40">
                 <Sidebar.Item>
                     <Link href="/profile" className={`${pathname === '/profile' ? 'text-white' : 'text-slate-400'} flex items-center gap-2 `}>
                         <FiUser className='text-3xl' />
-                        Profile
                     </Link>
                 </Sidebar.Item>
 
-                <Sidebar.Item>
-                    {/* <Link href="/login" className={`${pathname === '/login'? 'text-white' : 'text-slate-400'} flex items-center gap-2 `}>
-                        <ButtonLogout />
-                    </Link> */}
+                <Sidebar.Item className="text-white font-normal">
                     <ButtonLogout />
                 </Sidebar.Item>
             </Sidebar.Footer>
