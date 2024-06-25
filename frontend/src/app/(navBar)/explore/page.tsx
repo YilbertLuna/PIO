14a11ff76bd8c6776a5082096a784e1660ec39b7
YilbertLuna <yilbert.luna.e@gmail.com>
2024-06-25 02:40:15 -0400

    ci(front): errors were evaluated when searching for a user

--- frontend/src/app/(navBar)/explore/page.tsx
+++ frontend/src/app/(navBar)/explore/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image"
 import { useState, useEffect, useRef } from "react"
+import { FiSearch } from "react-icons/fi";
 
 import { User } from "@/interface"
 
@@ -9,15 +10,25 @@ import { User } from "@/interface"
 export default function Explore(): JSX.Element {
 
     const [users, setUsers] = useState<User[]>([])
-    const [search, setSearch] = useState('')
-    // const [isSearching, setIsSearching] = useState(true)
     const [foundUsers, setFoundUsers] = useState<User[]>([])
+    const [search, setSearch] = useState<string>()
+    const [isOk, setIsOk] = useState<boolean>(false)
+    const [empiti, setEmpiti] = useState<boolean>(false)
     const searchUserRef = useRef<HTMLInputElement>(null)
 
     function submitInputValue() {
+        if(searchUserRef.current?.value.trim() === '' || searchUserRef.current?.value === undefined) {
+            setEmpiti(true)
+            setIsOk(false)
+            return
+        }
+
         setSearch(String(searchUserRef.current?.value))
+        setIsOk(true)
+        setEmpiti(false)
+        
     }
-    
+
     useEffect(() => {
 
         async function getUsers() {
@@ -32,11 +43,11 @@ export default function Explore(): JSX.Element {
         }
 
         getUsers()
-
+        
         setFoundUsers(users.filter(user => user.name.includes(`${search}`)))
 
-    }, [search, users])
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [search])
 
     return(
         <main className="flex flex-col justify-center items-center space-y-10 md:ml-40 ml-4 mr-4 mt-20">
@@ -47,30 +58,30 @@ export default function Explore(): JSX.Element {
                         e.preventDefault()
                         submitInputValue()
                     }
-                } className="relative h-10 w-72 ">
-                    <input ref={searchUserRef} placeholder="search user"
-                    className="peer h-full w-full rounded-[7px] !border !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50" />
-                    <button type="submit" className="bg-button text-white p-2 rounded-lg text-base">search</button>
+                } className="h-10 w-72 bg-white rounded-xl">
+                    <div className="flex flex-row justify-center items-center gap-3">
+                        <input ref={searchUserRef} placeholder="search user"
+                        className="px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 focus:outline-none" />
+                        <button type="submit" className="bg-button text-black p-2 rounded-lg text-base focus:outline-none"><FiSearch /></button>
+                    </div>
                 </form>
             </div>
 
-            <div className="flex flex-col justify-center items-center w-80 sm:w-[460px] space-y-5 shadow-md p-10 rounded-xl bg-clip-border bg-color1">
-                {foundUsers.map(users => (
+            {empiti && <p className="font-semibold text-red-600 text-center">Enter user</p>}
+
+            {isOk && <div className="flex flex-col justify-center items-center w-80 sm:w-[460px] space-y-5 shadow-md p-10 rounded-xl bg-clip-border bg-color1">
+                {foundUsers.length === 0 && <p className="font-semibold text-red-600 text-center">User not found</p>}
+              
+                {foundUsers?.map(users => (
                     <div key={users.id} className="flex flex-row justify-start items-center w-72 space-x-10 px-10">
                         <figure>
                             <Image src="/FotoPerfil.jpg" alt="foto perfil" width={50} height={50} className="rounded-full"/>
                         </figure>
 
                         <h1 className="text-start">{users.name}</h1>
-
-                        {/* <button 
-                        className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-color3 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20  focus:shadow-none active:opacity-[0.85] active:shadow-none"
-                        type="submit">Follow</button> */}
                     </div>
                 ))}
-                
-            </div>
-
+            </div>}
         </main>
     )
 }
