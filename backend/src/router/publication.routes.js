d99eac4f0153b843c1530e313b3fe84d1b1569a4
YilbertLuna <yilbert.luna.e@gmail.com>
2024-07-04 15:38:55 -0400

    feat(backend): request to like was added

--- backend/src/router/publication.routes.js
+++ backend/src/router/publication.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authRequired } from "../middleware/validatorToken.js";
-import { createdPublication, deletePublication, getMyPublications, getPublications } from "../controllers/publications.controller.js";
+import { createdPublication, deletePublication, getMyPublications, getPublications, likePublication } from "../controllers/publications.controller.js";
 
 const publicationsRouter = Router()
 
@@ -9,6 +9,8 @@ publicationsRouter.get('/getpublications', authRequired, getPublications)
 publicationsRouter.get('/getMyPublications', authRequired, getMyPublications)
 
 publicationsRouter.post('/createPublication', authRequired, createdPublication)
+
+publicationsRouter.post('/like/:id', authRequired, likePublication)
 
 publicationsRouter.delete('/deletePublication/:id', authRequired, deletePublication)
 
